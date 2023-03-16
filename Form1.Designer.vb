3a07a98a2be7ab8bffc27375157afb1850881fb7
Nyeisha Pace <nyeisha.j.pace@gmail.com>
2023-03-15 21:50:15 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,208 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblConverter = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblMeters = New System.Windows.Forms.Label()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.rbInchesToMeters = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 27)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(277, 212)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Font = New System.Drawing.Font("Constantia", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnConvert.Location = New System.Drawing.Point(47, 369)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(206, 52)
+        Me.btnConvert.TabIndex = 1
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Constantia", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(300, 369)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(206, 52)
+        Me.btnClear.TabIndex = 2
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Constantia", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(552, 369)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(206, 52)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblConverter
+        '
+        Me.lblConverter.AutoSize = True
+        Me.lblConverter.Font = New System.Drawing.Font("Constantia", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblConverter.Location = New System.Drawing.Point(346, 40)
+        Me.lblConverter.Name = "lblConverter"
+        Me.lblConverter.Size = New System.Drawing.Size(252, 42)
+        Me.lblConverter.TabIndex = 4
+        Me.lblConverter.Text = "Converter App"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Constantia", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(357, 110)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(194, 52)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Enter a value and " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "choose conversion"
+        '
+        'lblMeters
+        '
+        Me.lblMeters.AutoSize = True
+        Me.lblMeters.Font = New System.Drawing.Font("Constantia", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMeters.Location = New System.Drawing.Point(401, 323)
+        Me.lblMeters.Name = "lblMeters"
+        Me.lblMeters.Size = New System.Drawing.Size(128, 26)
+        Me.lblMeters.TabIndex = 6
+        Me.lblMeters.Text = "10 meters is"
+        '
+        'txtInput
+        '
+        Me.txtInput.BackColor = System.Drawing.Color.DarkSlateBlue
+        Me.txtInput.Font = New System.Drawing.Font("Constantia", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtInput.ForeColor = System.Drawing.Color.White
+        Me.txtInput.Location = New System.Drawing.Point(611, 142)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(100, 21)
+        Me.txtInput.TabIndex = 7
+        Me.txtInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'ListBox1
+        '
+        Me.ListBox1.BackColor = System.Drawing.Color.DarkSlateBlue
+        Me.ListBox1.Font = New System.Drawing.Font("Constantia", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 26
+        Me.ListBox1.Items.AddRange(New Object() {"Convert Measurement"})
+        Me.ListBox1.Location = New System.Drawing.Point(406, 198)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(268, 108)
+        Me.ListBox1.TabIndex = 8
+        '
+        'rbInchesToMeters
+        '
+        Me.rbInchesToMeters.AutoSize = True
+        Me.rbInchesToMeters.BackColor = System.Drawing.Color.DarkSlateBlue
+        Me.rbInchesToMeters.Font = New System.Drawing.Font("Constantia", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rbInchesToMeters.Location = New System.Drawing.Point(416, 234)
+        Me.rbInchesToMeters.Name = "rbInchesToMeters"
+        Me.rbInchesToMeters.Size = New System.Drawing.Size(135, 22)
+        Me.rbInchesToMeters.TabIndex = 9
+        Me.rbInchesToMeters.TabStop = True
+        Me.rbInchesToMeters.Text = "Inches to Meters"
+        Me.rbInchesToMeters.UseVisualStyleBackColor = False
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.BackColor = System.Drawing.Color.DarkSlateBlue
+        Me.RadioButton2.Font = New System.Drawing.Font("Constantia", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton2.Location = New System.Drawing.Point(416, 268)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(135, 22)
+        Me.RadioButton2.TabIndex = 10
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Meters to Inches"
+        Me.RadioButton2.UseVisualStyleBackColor = False
+        '
+        'lblOutput
+        '
+        Me.lblOutput.AutoSize = True
+        Me.lblOutput.Font = New System.Drawing.Font("Constantia", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOutput.Location = New System.Drawing.Point(521, 328)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(58, 19)
+        Me.lblOutput.TabIndex = 11
+        Me.lblOutput.Text = "Label3"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.MediumPurple
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.rbInchesToMeters)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.lblMeters)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblConverter)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents btnConvert As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblConverter As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblMeters As Label
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents rbInchesToMeters As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents lblOutput As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,52 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblOutput.Text = ""
+        lblOutput.Anchor = AnchorStyles.None
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtInput.Text = ""
+        lblOutput.Text = ""
+
+        rbInchesToMeters.Checked = True
+        txtInput.Focus()
+
+    End Sub
+
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        Dim inputValue As Double
+
+        If Not Double.TryParse(txtInput.Text, inputValue) Then
+
+            MsgBox("Please enter a valid number.", MsgBoxStyle.Exclamation, "Error")
+            txtInput.Focus()
+            Return
+        End If
+
+        If inputValue <= 0 Then
+
+            MsgBox("Please enter a positive number.", MsgBoxStyle.Exclamation, "Error")
+            txtInput.Focus()
+            Return
+        End If
+
+        Dim convertedValue As Double
+
+        If rbInchesToMeters.Checked Then
+            convertedValue = inputValue * 0.0254
+        Else
+            convertedValue = inputValue / 0.0254
+        End If
+
+        If convertedValue < 0 Then
+            MsgBox("The converted value cannot be negative.", MsgBoxStyle.Exclamation, "Error")
+            Return
+        End If
+
+        lblOutput.Text = convertedValue.ToString("F2") & Space(2) & "inches."
+    End Sub
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+End Class
